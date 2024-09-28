17e45ee35f7324f3a26688406647791aa99004e0
khagag <kareem.m.hagag@gmail.com>
2024-09-28 21:24:33 +0300

    initial commit

---
+++ src/main/java/com/kmh/liberary/LiberaryRentalApplication.java
@@ -0,0 +1,13 @@
+package com.kmh.liberary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiberaryRentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiberaryRentalApplication.class, args);
+	}
+
+}
